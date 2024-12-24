16492a7fe39dec52b56ef87a04e0311265d41d1d
meleek21 <maloukmelliti@gmail.com>
2024-12-25 00:10:36 +0100

    Implemented Room Database and Repositories
    * Created entities (User, Category, Expense, Budget)
    * Implemented DAOs and Repositories
    * Established data layer foundation

---
+++ app/src/main/java/com/example/expencetracker/data/RoomDataBase/ExpenseTrackerDataBase.java
@@ -0,0 +1,36 @@
+package com.example.expencetracker.data.RoomDataBase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.expencetracker.data.DOAs.BudgetDAO;
+import com.example.expencetracker.data.DOAs.CategoryDAO;
+import com.example.expencetracker.data.DOAs.ExpenseDAO;
+import com.example.expencetracker.data.DOAs.UserDAO;
+import com.example.expencetracker.data.Entities.Budget;
+import com.example.expencetracker.data.Entities.Category;
+import com.example.expencetracker.data.Entities.Expense;
+import com.example.expencetracker.data.Entities.User;
+
+@Database(entities = {Expense.class, Category.class, Budget.class, User.class}, version = 1)
+public abstract class ExpenseTrackerDataBase extends RoomDatabase {
+
+    private static ExpenseTrackerDataBase INSTANCE;
+
+    public static ExpenseTrackerDataBase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ExpenseTrackerDataBase.class, "expense_tracker_db")
+                    .build();
+        }
+        return INSTANCE;
+    }
+
+    public abstract ExpenseDAO expenseDAO();
+    public abstract CategoryDAO categoryDAO();
+    public abstract BudgetDAO budgetDAO();
+    public abstract UserDAO userDAO();
+}
+
